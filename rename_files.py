76b9dd41fa91f407ecec1571039e37b335299a62
Letty <lettycheungzl@gmail.com>
2018-01-08 23:51:15 +0800

    The program, it cannot find the file specified.

--- rename_files.py
+++ rename_files.py
@@ -8,6 +8,9 @@ def rename_files():
     # (1) get file names from a folder
     file_list = os.listdir("/Users/letty/Downloads/prank")
     print(file_list)
-    # (2) for each file, rename filename
 
+    # (2) for each file, rename filename
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+        
 rename_files()
