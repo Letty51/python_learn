7ab73af205e5a2b4ebd733b6fb0d7004cec28ae9
Letty <lettycheungzl@gmail.com>
2018-01-09 00:29:33 +0800

    print Old Name and New Name

--- rename_files.py
+++ rename_files.py
@@ -16,7 +16,10 @@ def rename_files():
 
     # (2) for each file, rename filename
     for file_name in file_list:
+        print("Old Name - " + file_name)
         os.rename(file_name, file_name.translate(None, "0123456789"))
+        #print("Old Name - " + file_name)
+        print("New Name - " + file_name.translate(None, "0123456789"))
 
     os.chdir(saved_path)
 
