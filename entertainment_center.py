88294ebcfcbe5644ec3416459118c546888bf5fa
Letty <lettycheungzl@gmail.com>
2018-01-11 10:12:13 +0800

    media.py creates a class Movie. entertainment_center.py creates some instances of Movie. fresh_tomatoes.py had a function inside called open_movies_page. It receives a list of movies, and then, creates and opens an HTMLfile or a webpage that showcases those movies. By the way, it stores this HTML file, in the exact same folder, as all of other movie program files.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,20 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story", 
                         "A story of a boy and his toys that come to life", 
-                        "http://upload.wikimedia.org//wikipedia/en/1/13/Toy_Story.jpg", 
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+                        "https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg", 
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar", 
+                     "A marine on an alien planet", 
+                     "https://en.wikipedia.org/wiki/Avatar_(2009_film)#/media/File:Avatar-Teaser-Poster.jpg", 
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+movies = [toy_story, avatar]
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -7,9 +7,14 @@
 #def show_info(title, storyline, release_date, rating, youtube_url, director, box_office):
     # Print movie information
 
+import webbrowser
+
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_youtube_url =trailer_youtube
+        self.trailer_youtube_url = trailer_youtube
+    
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
